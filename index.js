f91e131bfe59b3f6b003867bbfb54b1e1000d79c
Aka'aba Musa Akidi <onlinemraam@gmail.com>
2023-03-19 12:14:11 +0100

    change /login to /test

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const app = express();
 
-const { User, Category } = require("./models");
+const { Category } = require("./models");
 
-app.get("/login", async function (req, res) {
+app.get("/test", async function (req, res) {
   const createCategory = await Category.create({
     name: `ICT`,
   });
 
-  const category = await Category.findOne({ where: { name: "ICT" } });
+  const parentCategory = await Category.findOne({ where: { name: "ICT" } });
+  const childCategory = await parentCategory.createChild({
+    name: "Child",
+  });
 
-  const root = await Category.findOne({ where: { name: "Root" } });
-  // find and create
-  const child1 = await root.createChild({ name: "this is another child 1" });
-  const child2 = await root.createChild({ name: "Child here is her " });
-  const grandchild = await child1.createChild({ name: "Grandchild" });
-
-  // const descendents = await category.getDescendents();
+  const children = await parentCategory.getChildren();
+  const parent = await childCategory.getParent();
+  const descendants = await parentCategory.getDescendents();
+  const ancestors = await childCategory.getAncestors();
 
   if (createCategory) {
     res.send({
       working: true,
-      category,
-      root,
-      child1,
-      child2,
+      parent,
+      children,
+      descendants,
+      ancestors,
     });
   }
 });
